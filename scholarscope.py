a438b201689d01f3ef51e29e799d80d6a3c00700
JoeCUK <joseph.cutteridge@doctors,org.uk>
2024-12-20 00:27:21 +0000

    Additional comments in scholarscope

--- scholarscope.py
+++ scholarscope.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 @app.route("/api/publications", methods=["GET"])
 def get_publications():
     author = request.args.get("author")
-    # Mocked data for demonstration
+    # Mocked data 
     data = simulate_fetch_publication_data(author)
     return jsonify(data)
 
 def simulate_fetch_publication_data(author):
     # This function simulates returning data for a given author.
-    # You can replace this with actual logic to fetch data from a database or external source.
+    # To be replaced by actuall logic to fetch data from PubMed API
     history = {2020: 2, 2021: 3, 2022: 4}
     projections = {2023: 5, 2024: 6}
     return {"history": history, "projections": projections}
